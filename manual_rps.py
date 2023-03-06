546cfb81c6e1cbebe7c5daf95fce944c6724c933
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-06 14:47:59 +0000

    Adds a 'manual.py' file which takes a user input and a random choice by computer

---
+++ manual_rps.py
@@ -0,0 +1,10 @@
+import random
+
+def get_computer_choice():
+    option_list = ["Rock", "Paper", "Scissors"]
+    
+    return random.choice(option_list)
+
+
+def get_user_choice():
+    user_choice = input("Enter your choice: ")
