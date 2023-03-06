99e66d925bd4be1b750d1dc1bc9ab3abfb3dabd3
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-06 14:58:19 +0000

    Adds a function get_winner() which has if-elif-else statement rules for deciding the winner

--- manual_rps.py
+++ manual_rps.py
@@ -2,11 +2,30 @@ import random
 
 def get_computer_choice():
     option_list = ["Rock", "Paper", "Scissors"]
+    computer_choice = random.choice(option_list)
     
-    return random.choice(option_list)
+    return computer_choice
 
 
 def get_user_choice():
     user_choice = input("Enter your choice: ")
 
     return user_choice
+
+
+def get_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("It is a tie!")
+
+    elif user_choice == "Rock" and computer_choice == "Scissors":
+        print("You won!")
+
+    elif user_choice == "Paper" and computer_choice == "Rock":
+        print("You won!")
+
+    elif user_choice == "Scissors" and computer_choice == "Paper":
+        print("You won!")
+
+    else:
+        print("You lost")
+
